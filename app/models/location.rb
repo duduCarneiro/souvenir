16283d608b7a4ab249b2751ef3b1df9de9dc72de
dudu carneiro <dumaniaoctavio@hotmail.com>
2021-06-07 17:45:20 -0300

    basic stuff

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,5 @@
 class Location < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  has_many :albums
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :locations do
+    resources :albums, only: [:new, :create]
+    resources :comments, only: [:new, :create]
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
