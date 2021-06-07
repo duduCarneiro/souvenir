f7f6621e6d9f7d6a1ff31cc7104e61d825c4fa26
dudu carneiro <dumaniaoctavio@hotmail.com>
2021-06-07 20:29:40 -0300

    Address input autocomplete

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,7 +4,10 @@ class LocationsController < ApplicationController
     @markers = @locations.geocoded.map do |location|
         {
           lat: location.latitude,
-          lng: location.longitude
+          lng: location.longitude,
+          info_window: render_to_string(partial: "info_window", locals: { location: location })
+        #   image_url: helpers.asset_url("car_icon.png")
+
         }
     end
   end
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,16 +5,19 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+    map.fitBounds(bounds, { padding: 50, maxZoom: 11, duration: 1000 });
 };
 
 const addMarkersToMap = (map, markers) => {
     markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window); // add this
+  
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup) // add this
         .addTo(map);
     });
-};
+  };
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
