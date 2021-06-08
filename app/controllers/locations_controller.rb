3328b4852f14bb2adbf540b61fede74b60b153e6
joao sampaio <joaomarcosemgp@gmail.com>
2021-06-08 13:06:50 -0300

    views basicas de new e show e modificaçõe de e ações no locations_controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,31 @@
 class LocationsController < ApplicationController
+
+
+  def show
+    @location = Location.find(params[:id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @location = Location.new
+    @location.albums.build
+    @location.comments.build
+  end
+
+  def create
+    
+    @location = Location.new(location_params)
+    @location.user = current_user
+    if @location.save
+      redirect_to location_path(@location), notice: "Experience created"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def location_params
+      params.require(:location).permit(:name, :address, albums_attributes: [photos: []], comments_attributes: [:text])
+  end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,6 @@ class Location < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :albums
+  accepts_nested_attributes_for :albums
+  accepts_nested_attributes_for :comments
 end
