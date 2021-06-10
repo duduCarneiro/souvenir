a0878789f317d374cb22d12aaeb2f2071bbb8ad1
dudu carneiro <dumaniaoctavio@hotmail.com>
2021-06-10 15:01:03 -0300

    everything working

--- app/javascript/plugins/init_Ialize.js
+++ app/javascript/plugins/init_Ialize.js
@@ -1,8 +1,8 @@
 
 function initIalize() {
-    const earthDiv = document.getElementById("earth_div")
-    if (earthDiv) {
-      var earth = new WE.map('earth_div');
+  const earthDiv = document.getElementById("earth_div")
+  if (earthDiv) {
+    var earth = new WE.map('earth_div');
     earth.setView([46.8011, 8.2266], 2);
     WE.tileLayer('https://webglearth.github.io/webglearth2-offline/{z}/{x}/{y}.jpg', {
       tileSize: 256,
@@ -19,17 +19,17 @@ function initIalize() {
     console.log(markersarray);
 
     JSON.parse(markersarray).forEach((pin) => {
-        console.log(pin.idlocation);
-        WE.marker([pin.lat, pin.lng], pin.ppic, 150, 50).addTo(earth);
-      });
+      console.log(pin.idlocation);
+      WE.marker([pin.lat, pin.lng], pin.ppic, 150, 50).addTo(earth);
+    });
 
 
     console.log("done");
     var markerCustom = WE.marker([500, -9], '/img/logo-webglearth-white-100.png', 100, 24).addTo(earth);
 
-    }
-    
-}
-  
+  }
 
-    export { initIalize };
+}
+
+
+export { initIalize };
