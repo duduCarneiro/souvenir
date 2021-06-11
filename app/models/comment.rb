2d91c95e40bf98cfa601ba262e602768145623aa
dudu carneiro <dumaniaoctavio@hotmail.com>
2021-06-11 00:12:10 -0300

    searchbardone

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :location
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :date ],
+    associated_against: {
+      location: [ :address, :name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
