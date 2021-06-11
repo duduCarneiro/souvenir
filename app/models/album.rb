332c7f04a593831f94b68cad86c92c7d5f55285f
dudu carneiro <dumaniaoctavio@hotmail.com>
2021-06-10 21:20:04 -0300

    search doing

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,13 @@ class Album < ApplicationRecord
   belongs_to :location
   has_many_attached :photos
   # validates :photos, presence: true
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :date ],
+    associated_against: {
+      location: [ :address, :name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
