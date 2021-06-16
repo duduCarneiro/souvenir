f936ba65dbc192f942aceb9e536f0f9adbb87100
dudu carneiro <dumaniaoctavio@hotmail.com>
2021-06-16 17:53:06 -0300

    done footer

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -14,13 +14,13 @@ class LocationsController < ApplicationController
 
     @markers = @locations.geocoded.map do |location|
       {
-      # idlocation: location.id,
       lat: location.latitude,
       lng: location.longitude,
       kpic: location.albums.first.photos.first.try(:key),
-      # image_url: helpers.asset_url('cam_icon.png')
       redirect_to: location_url(location),
-      image_url: ''
+      image_url: '',
+      id: location.id # we add the id of the flat in the hash
+
       # image_url: helpers.asset_url(cl_image_path('6tgvujx90x75374ypiiy1rq9qted'))
       # image_url: cl_image_path('6tgvujx90x75374ypiiy1rq9qted')
       # image_url: helpers.asset_url(cl_image_path('6tgvujx90x75374ypiiy1rq9qted'))
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -30,6 +30,16 @@ const addMarkersToMap = (map, markers) => {
   });
 };
 
+// HOOVER album when cursor on the marker
+
+const toggleCardHighlighting = (event) => {
+  // We select the card corresponding to the marker's id
+  const card = document.querySelector(`[data-location-id="${event.currentTarget.dataset.markerId}"]`);
+  // Then we toggle the class "highlight github" to the card
+  card.classList.toggle('highlight');
+}
+
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -52,7 +62,24 @@ const initMapbox = () => {
     //     .setLngLat([ marker.lng, marker.lat ])
     //     .addTo(map);
     // });
+    ////////////////////////////////////////////////////////////////////////////////////////
+    const mapMarkers = [];
+    markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
 
+      const newMarker = new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(map);
+      mapMarkers.push(newMarker)
+      // We use the "getElement" funtion provided by mapbox-gl to access to the marker's HTML an set an id
+      newMarker.getElement().dataset.markerId = marker.id;
+      // Put a microphone on the new marker listening for a mouseenter event
+      newMarker.getElement().addEventListener('mouseenter', (e) => toggleCardHighlighting(e) );
+      // We put a microphone on listening for a mouseleave event
+      newMarker.getElement().addEventListener('mouseleave', (e) => toggleCardHighlighting(e) );
+    });
+    ///////////////////////////////////////////////////////////////////////////////////////
     addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
 
