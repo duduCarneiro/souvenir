df30be3782189f4a5cd0e52bf71ecaaf662f080e
dudu carneiro <dumaniaoctavio@hotmail.com>
2021-06-16 19:31:38 -0300

    carousel fixed

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -18,8 +18,8 @@ class LocationsController < ApplicationController
       lng: location.longitude,
       kpic: location.albums.first.photos.first.try(:key),
       redirect_to: location_url(location),
-      image_url: '',
-      id: location.id # we add the id of the flat in the hash
+      image_url: ''
+      # id: location.id # we add the id of the flat in the hash
 
       # image_url: helpers.asset_url(cl_image_path('6tgvujx90x75374ypiiy1rq9qted'))
       # image_url: cl_image_path('6tgvujx90x75374ypiiy1rq9qted')
