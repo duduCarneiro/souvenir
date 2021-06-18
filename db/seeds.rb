98fa66c0994e8277f7fc5b932ca2643991791467
dudu carneiro <dumaniaoctavio@hotmail.com>
2021-06-18 17:11:26 -0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,7 +36,7 @@ images_montaigne = ['https://www.dreameratheart.org/wp-content/uploads/2019/12/d
         'https://images.unsplash.com/photo-1570118449326-1e20ac059784?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI0MDQwNzUz&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600'
         ]
 
-images_invalide = ['https://www.motherofalltrips.com/wp-content/uploads/2013/04/Paris-with-kids-Invalides-armor.jpg',
+images_invalide = ['https://cdn.north-westbrides.com/wp-content/uploads/2018/10/21165104/people-3755342_960_720.jpg',
         'https://res.cloudinary.com/dii4d3v8r/image/upload/v1624044579/w12ep5xijipebwd7wlz8ng2p79j6.jpg',
         'https://images.unsplash.com/photo-1607453998774-d533f65dac99?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI0MDM3ODMy&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600',
         'https://images.unsplash.com/photo-1504151932400-72d4384f04b3?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI0MDQyMTQz&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600',
@@ -45,8 +45,8 @@ images_invalide = ['https://www.motherofalltrips.com/wp-content/uploads/2013/04/
         
 images_notredame = ['https://www.thelocal.fr/wp-content/uploads/2019/04/f41445a26f231a896a5ea3d92808ce4b29a8c9089eb15c5f03e859f608a8b547.jpg',
         'https://viagemeturismo.abril.com.br/wp-content/uploads/2016/12/1134863761.jpg?quality=70&strip=info&w=680&h=453&crop=1',
-        'https://res.cloudinary.com/dii4d3v8r/image/upload/v1624044963/5tbsa5fr0h5tctp35w2wt9ljb5w8.jpg',
-        'https://f.i.uol.com.br/fotografia/2018/09/06/15362720845b91a6d461ee8_1536272084_3x2_md.jpg',
+        'https://media.tacdn.com/media/attractions-splice-spp-674x446/06/6e/d1/90.jpg',
+        'https://familytwistdev-images.s3.eu-west-3.amazonaws.com/activity/1546247204989Notre_Dame_Medieval_ParisFamilyTwist_Paris_Notre_Dame_Medieval_Paris_11x.jpg',
         'https://www.catholicregister.org/media/k2/items/cache/087381c44d8fe74529d92a4430f72e21_XL.jpg'
         ]
 
@@ -68,7 +68,7 @@ index_of_array_notredame = 0
     album.photos.attach(io: file, filename: 'some-image.jpg')
     sum +=1
     index_of_array_montaigne +=1
-    puts "(#{sum} of 24 done)"
+    puts "(#{sum} of 23 done)"
 end
 
 
@@ -79,7 +79,7 @@ end
     album.photos.attach(io: file, filename: 'some-image.jpg')
     sum +=1
     index_of_array_invalide +=1
-    puts "(#{sum} of 24 done)"
+    puts "(#{sum} of 23 done)"
 end
 
 5.times do
@@ -89,7 +89,7 @@ end
     album.photos.attach(io: file, filename: 'some-image.jpg')
     sum +=1
     index_of_array_notredame +=1
-    puts "(#{sum} of 24 done)"
+    puts "(#{sum} of 23 done)"
 end
 
 # ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -100,7 +100,7 @@ end
     file = open(images[0])
     album.photos.attach(io: file, filename: 'some-image.jpg')
     sum +=1
-    puts "(#{sum} of 24 done)"
+    puts "(#{sum} of 23 done)"
 end
 
 2.times do
@@ -109,7 +109,7 @@ end
     file = open(images[1])
     album.photos.attach(io: file, filename: 'some-image.jpg')
     sum +=1
-    puts "(#{sum} of 24 done)"
+    puts "(#{sum} of 23 done)"
 end
 
 2.times do
@@ -118,7 +118,7 @@ end
     file = open(images[2])
     album.photos.attach(io: file, filename: 'some-image.jpg')
     sum +=1
-    puts "(#{sum} of 24 done)"
+    puts "(#{sum} of 23 done)"
 end
 
 2.times do
@@ -127,7 +127,7 @@ end
     file = open(images[3])
     album.photos.attach(io: file, filename: 'some-image.jpg')
     sum +=1
-    puts "(#{sum} of 24 done)"
+    puts "(#{sum} of 23 done)"
 end
 
 
@@ -146,11 +146,14 @@ verbis = ['This place is where I met Jorge for the first time, we drank a beer a
 'I met my idol Mastodon for the very first time on this street',
 'I managed to do my first backflip with a stranger I met, called Matt and we became good friends.',
 'Beautiful place, visiting for the 5th time and still amazes me',
-'In love with the culture and the architecture, I need to organize this trip in the years to come.']
+'In love with the culture and the architecture, I need to organize this trip in the years to come.',
+'The past beats inside me like a second heart.',
+"What i like about photographs is that they capture a moment that's gone forever, impossible to reproduce.",
+'No matter how much suffering you went through, you never wanted to let go of those memories.']
 
 i = 0
 verbis.each do |element|
-    comment = Comment.create!(date: rand(24).months.ago, text: element, location_id: rand(Location.first.id..Location.last.id))
+    comment = Comment.create!(date: rand(24).months.ago, text: element, location_id: rand(Location.first.id..Location.third.id))
     puts "comment id: #{comment.id}- comment: #{comment.text}- date: #{comment.date}- location: #{comment.location_id}"
     i +=1
     puts "(#{i} of #{verbis.count} done)"
